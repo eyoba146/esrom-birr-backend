71064e1dc65921245f1a8d21fe1be0e1ca0ebf5e
eyoba146 <eyobadamu146@gmail.com>
2026-06-28 04:52:03 -0300

    feat: verify waiter portal and offline order flow working

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./auth.routes.js";
 import employeeRoutes from "./employee.routes.js";
+import waiterRoutes from "./waiter.routes.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/employee", employeeRoutes);
+router.use("/waiter", waiterRoutes);
 
 export default router;
