bf95271deda13e28d9ef5069543303da801f0ef4
eyoba146 <eyobadamu146@gmail.com>
2026-06-28 05:16:05 -0300

    feat: add monthly report generation in XLSX, PDF and CSV

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./auth.routes.js";
 import employeeRoutes from "./employee.routes.js";
 import waiterRoutes from "./waiter.routes.js";
 import cafeRoutes from "./cafe.routes.js";
+import reportRoutes from "./report.routes.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRoutes);
 router.use("/employee", employeeRoutes);
 router.use("/waiter", waiterRoutes);
 router.use("/cafe", cafeRoutes);
+router.use("/reports", reportRoutes);
 
 export default router;
