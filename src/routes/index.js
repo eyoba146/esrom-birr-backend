2cd8e84dd2314911123682576c7488927dc8de00
selam-mbk1 <selamawitbasaznew@gmail.com>
2026-06-29 19:45:10 +1200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,16 @@
+import express from "express";
+import authRoutes from "./auth.routes.js";
+import employeeRoutes from "./employee.routes.js";
+import waiterRoutes from "./waiter.routes.js";
+import cafeRoutes from "./cafe.routes.js";
+import reportRoutes from "./report.routes.js";
 
+const router = express.Router();
+
+router.use("/auth", authRoutes);
+router.use("/employee", employeeRoutes);
+router.use("/waiter", waiterRoutes);
+router.use("/cafe", cafeRoutes);
+router.use("/reports", reportRoutes);
+
+export default router;
