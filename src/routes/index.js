c175a8c2552ba67896959cfafdc7717636890361
selam-mbk1 <selamawitbasaznew@gmail.com>
2026-06-29 19:26:06 +1200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,1 @@
-import express from "express";
-import authRoutes from "./auth.routes.js";
-import cafeRoutes from "./cafe.routes.js";
-import companyRoutes from "./company.routes.js";
-import notificationRoutes from "./notification.routes.js";
 
-const router = express.Router();
-
-router.use("/auth", authRoutes);
-router.use("/cafe-manager", cafeRoutes);
-router.use("/company-manager", companyRoutes);
-router.use("/notifications", notificationRoutes);
-
-export default router;
